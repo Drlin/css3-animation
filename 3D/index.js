d5d6d52806ed1d9c780c863a0162f52165f422ed
kzkxdzt <ljlbxy@126.com>
2017-02-04 11:38:03 +0800

    update

--- 3D/index.js
+++ 3D/index.js
@@ -1,9 +1,49 @@
 (() => {
 	function Animation() {
-		
+		this.init();
+		this.page = 1;
+		this.curPage = document.getElementById(`page${this.page}`);
 	}
 	Animation.prototype = {
-
+		init() {
+			this.bindEvent();
+		},
+		bindEvent() {
+			this.domTransform();
+		},
+		domTransform() {
+			let dom = document.querySelector('.btn-group');
+			dom.addEventListener('click', (e) => {
+				let targetId = e.target.id;
+				console.log(targetId === "next");
+				if (targetId === "next") {
+					this.next();
+				} else {
+					this.pre();
+				}
+				
+			})
+		},
+		next() {
+			if (this.page >= 6) {
+				return;
+			}
+			var curPage = document.getElementById(`page${this.page}`);
+			curPage.style.transform = 'rotateX(-90deg)';
+			this.page++;
+			var nextPage = document.getElementById(`page${this.page}`);
+			nextPage.style.transform = 'rotateX(0deg)';
+		},
+		pre() {
+			if (this.page <= 1) {
+				return;
+			}
+			var curPage = document.getElementById(`page${this.page}`);
+			curPage.style.transform = 'rotateX(90deg)';
+			this.page--;
+			var nextPage = document.getElementById(`page${this.page}`);
+			nextPage.style.transform = 'rotateX(0deg)';
+		}
 	}
 	new Animation();
 })();
